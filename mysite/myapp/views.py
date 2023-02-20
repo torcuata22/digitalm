c9e00ebdc1cfcfc1465878ef7cfa60190d1b9d88
Marilyn Marquez <marilynmarquez@MMarquez.local>
2023-02-20 13:36:19 -0600

    checkout backend

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,6 +1,7 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.urls import reverse #added this as suggestion to avoid error, need to check
-from  .models import Product
+from  .models import Product, OrderDetail
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
 import stripe, json
@@ -21,7 +22,7 @@ def create_checkout_session(request, id):
      request_data = json.load(request.body)
      product = Product.objects.get(id=id)
      stripe.api_key = settings.STRIPE_SECRET_KEY
-     chekout_session = stripe.checkout.Session().create( #everything here is defined by Stripe
+     checkout_session = stripe.checkout.Session().create( #everything here is defined by Stripe
         customer_email = request_data['email'],
         payment_method_types=['card'],
         line_items=[
@@ -40,5 +41,17 @@ def create_checkout_session(request, id):
         success_url = request.build_absolute_uri(reverse('success')) + 
         "?session_id={CHECKOUT_SESSION_ID}",
         cancel_url = request.build_absolute_uri(reverse('failed')),
-
      )
+     #after checkout session is completed:
+     order = OrderDetail()
+     order.customer_email = request_data['email'] #get field contents from the request data
+     order.product = product #instantiated from Product class
+     order.stripe_payment_intent = checkout_session['payment_intent']#we get this from the checkout session we created above
+     order.amount = int(product.price)
+     order.save()
+     #this will not render any kind of page, it will instead return a JSON response
+     #to get the session_id, we make it return a JSON response:
+     return JsonResponse({'sessionId':checkout_session.id})
+
+def payment_success_view(request):
+    session_id = request.GET.get('session_id') #if it exists, then it is valid session. If not, session doesn't exist
