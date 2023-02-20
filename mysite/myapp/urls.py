501d94dfc2fe8b3dec00ed7bf17207163988ad48
Marilyn Marquez <marilynmarquez@MMarquez.local>
2023-02-20 14:13:32 -0600

    failed and success

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('product/<int:id>', views.detail, name='detail'),
+    path('success/', views.payment_success_view, name='success'),
+    path('failed/', views.failed, name='failed'),
     
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,6 +1,6 @@
-from django.http import JsonResponse
-from django.shortcuts import render
-from django.urls import reverse #added this as suggestion to avoid error, need to check
+from django.http import JsonResponse, HttpResponseNotFound
+from django.shortcuts import render, get_object_or_404, reverse
+#from django.urls import reverse #added this as suggestion to avoid error, need to check
 from  .models import Product, OrderDetail
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
@@ -55,3 +55,15 @@ def create_checkout_session(request, id):
 
 def payment_success_view(request):
     session_id = request.GET.get('session_id') #if it exists, then it is valid session. If not, session doesn't exist
+    if session_id is None:
+        return HttpResponseNotFound()
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    session = stripe.checkout.Session.retrieve(session_id)
+    order = get_object_or_404(OrderDetail, stripe_payment_intent = session.payment_intent)
+    order.has_paid = True
+    order.save()
+
+    return render(request, 'myapp/payment_success.html', {'order':order})
+
+def payment_failed_view(request):
+    return render(request, 'myapp/failed.html')
