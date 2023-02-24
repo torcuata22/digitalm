dbd75062faf08ecf3c4827261d0d8488feb747c2
Marilyn Marquez <marilynmarquez@MMarquez.local>
2023-02-24 14:38:03 -0600

    logout

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -16,5 +16,6 @@ urlpatterns = [
     path('dashboard',views.dashboard,name='dashboard'),
     path('register/',views.register,name='register'),
     path('login/',auth_views.LoginView.as_view(template_name='myapp/login.html'),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='myapp/logout.html'),name='logout'),
 
 ]
